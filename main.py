5550665b741c6b954a71127b4a299c2d8b09e4e2
TheYafen <theyafen@gmail.com>
2025-09-16 23:38:51 +0300

    ~

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import requests
 
 def get_public_ip():
-    try 
-    ip = requests.get("https://api.myip.com").text
+    try:
+        ##Another API: https://api.myip.com
+        ip = requests.get("https://api.ipify.org", timeout=5).text
+    except Exception as e:
+        print(f"Error getting IP: {e}")
+        return None
     return ip
+
+
 
 a = 1
---
+++ main_idle_in_venv.bat
@@ -0,0 +1,7 @@
+.\venv\Scripts\activate
+
+pause
+
+python -m idlelib.idle main.py
+
+pause
